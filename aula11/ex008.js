5c8f56791e17287dd0a8d5eb661fab32a9c70643
Angelo Braz Albini <102412820+Albini-Hi@users.noreply.github.com>
2023-06-15 06:12:56 -0300

    ex010

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,3 +1,7 @@
 
-var vel = 60.5
+var vel = 60
 console.log(`A velocidade do seu carro é ${vel} K,/h`)
+if (vel > 60) { // condição simples
+    console.log('Você ultrapassou o limite de velocidade permitida. MULTADO')
+}
+console.log('Dirirga sempre com segurança')
