2770274e22dc5e3c2519fb83c99004fe688edd71
Angelo Braz Albini <102412820+Albini-Hi@users.noreply.github.com>
2023-06-27 20:17:28 -0300

    ex15 fim

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -9,11 +9,41 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //menino
+                img.setAttribute('src','menino.png')
+            } else if ( idade < 21) {
+                //moço
+                img.setAttribute('src','moço.png')
+            } else if (idade < 50) {
+                //homem
+                img.setAttribute('src','homem.png')
+            } else {
+                //senhor
+                img.setAttribute('src','senhor.png')
+            }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //menina
+                img.setAttribute('src','menina.png')
+            } else if ( idade < 21) {
+                //moça
+                img.setAttribute('src','moça.png')
+            } else if (idade < 50) {
+                //mulher
+                img.setAttribute('src','mulher.png')
+            } else {
+                //senhora
+                img.setAttribute('src','senhora.png')
+            }
         } 
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
