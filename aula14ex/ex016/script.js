4a39274a01665209f1ea8dd2334a09d3a60d8667
Angelo Braz Albini <102412820+Albini-Hi@users.noreply.github.com>
2023-07-07 06:41:53 -0300

    tebtei

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,3 +1,9 @@
 function contar() {
-
+    var ini = document.getElementById('iniciotxt')
+    var fim = document.getElementById('fimtxt')
+    var res = document.getElementById('res')
+    var cont = res
+    while (ini + fim > res) do {
+        res.innerHTML('cont')
+    }
 }
