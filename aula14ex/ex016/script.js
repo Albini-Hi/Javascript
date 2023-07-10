f400ed66f9419f57fb78d5bc3f9fa2564a25888c
Angelo Braz Albini <102412820+Albini-Hi@users.noreply.github.com>
2023-07-10 19:58:19 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -3,17 +3,28 @@ function contar() {
     var fim = document.getElementById('fimtxt')
     var passo = document.getElementById('passotxt')
     var res = document.getElementById('res')
-    var cont = res
-
+    
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados')
+        window.alert('Impossível contar')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando:<br>'
         let i = Number(ini.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            for(let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} \u{1f449}`
+            }
+        }else {
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        }
+        res.innerHTML += ` \u{1f3c1}`        
     }
-    while (ini.Number + fim.Number > res) do {
-        res.innerHTML('cont')
-    }
+    
 }
