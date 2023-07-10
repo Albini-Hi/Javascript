77a50255c374c8bcceb41b16f1687956bf23887b
Angelo Braz Albini <102412820+Albini-Hi@users.noreply.github.com>
2023-07-10 06:13:54 -0300

    01

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,9 +1,19 @@
 function contar() {
     var ini = document.getElementById('iniciotxt')
     var fim = document.getElementById('fimtxt')
+    var passo = document.getElementById('passotxt')
     var res = document.getElementById('res')
     var cont = res
-    while (ini + fim > res) do {
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+    }
+    while (ini.Number + fim.Number > res) do {
         res.innerHTML('cont')
     }
 }
