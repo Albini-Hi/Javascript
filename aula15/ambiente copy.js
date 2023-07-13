983ee7bed1e1ccfa0f88fe63611685445ffc3474
Angelo Braz Albini <102412820+Albini-Hi@users.noreply.github.com>
2023-07-13 20:03:41 -0300

    s

---
+++ aula15/ambiente copy.js
@@ -0,0 +1,7 @@
+let num = [3,7,8,4,9]
+let pos = num.indexOf(8)
+if (pos == -1 ) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,2 +1,11 @@
-let num = [3,7,8,4]
-console.log(`Nosso vetor é o ${num}`)
+let num = [3,7,8,4,9]
+num.sort()
+/*console.log(`O vetor tem ${num.length} posições `)
+console.log(`O vetor na ordem crescente é ${num.sort()}`)
+for(let pos = 0 ; pos < num.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}*/
+
+for(let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
