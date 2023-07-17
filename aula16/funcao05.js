dadb092dcbf780434b4168419f4f2b8b3681b5e1
Angelo Braz Albini <102412820+Albini-Hi@users.noreply.github.com>
2023-07-17 19:37:22 -0300

    VARIAS function

---
+++ aula16/funcao05.js
@@ -0,0 +1,18 @@
+// RECURSIVIDADE
+
+function fatorial(n) {
+    if(n==1) {
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+console.log(fatorial(4))
+
+/*
+5! é a mesma coisa que 5x4x3x2x1
+5! = 5x4x3x2x1
+5! = 4x4!
+n! = n x (n-1)!
+1! = 1
+*/
