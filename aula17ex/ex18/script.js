702645ef7aa6c599d70231b4f22e572464c34022
Angelo Braz Albini <102412820+Albini-Hi@users.noreply.github.com>
2023-07-20 06:10:57 -0300

    erro

--- aula17ex/ex18/script.js
+++ aula17ex/ex18/script.js
@@ -1,7 +1,7 @@
 let num = document.querySelector('input#numtxt')
-var lista = document.getElementById('flista')
+let lista = document.getElementById('flista')
 let res = document.querySelector('div#res')
-var valores =[]
+let valores =[]
 
 function numero(n) {
     if(Number(n) >= 1 && Number(n) <= 100) {
@@ -21,8 +21,25 @@ function inlista(n, l) {
 
 function adicionar(){
     if(numero(num.value) && !inlista(num.value, valores)) {
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+
+}
+
+function finalizar() {
+    if(valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+
+        res.innerHTML = ''
+        //res.innerHTML += `<p>Ao todo, temos ${} número de elementos</p>` tem um erro aqui
     }
 }
