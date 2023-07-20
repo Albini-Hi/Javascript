f46e51558a7301693044620102edad9f68b3e31e
Angelo Braz Albini <102412820+Albini-Hi@users.noreply.github.com>
2023-07-20 20:15:33 -0300

    Update script.js

--- aula17ex/ex18/script.js
+++ aula17ex/ex18/script.js
@@ -25,6 +25,7 @@ function adicionar(){
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
@@ -38,8 +39,17 @@ function finalizar() {
         window.alert('Adicione valores antes de finalizar!')
     } else {
         let tot = valores.length
-
+        let maior = valores[0]
+        let menor = valores[0]
+        for(let pos in valores) {
+            if(valores[pos] > maior)
+            maior = valores[pos]
+            if(valores[pos] < menor)
+            menor = valores[pos]
+        }
         res.innerHTML = ''
-        //res.innerHTML += `<p>Ao todo, temos ${} número de elementos</p>` tem um erro aqui
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
     }
 }
