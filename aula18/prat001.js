97d33492ddbff688c32a38e7e84fe2c74851521d
Angelo Braz Albini <102412820+Albini-Hi@users.noreply.github.com>
2023-08-09 05:31:13 -0300

    perda

---
+++ aula18/prat001.js
@@ -0,0 +1,25 @@
+/*let numero1 = 50
+let numero2 = numero1
+numero2 = 10
+numero1 = 5
+numero1 = 20
+numero1 = numero2
+numero1 += numero2 * numero1
+console.log(numero2)
+console.log(numero1)
+const num = 5;
+console.log(num)
+let num1 = 6
+console.log(num1)
+var num2 = 8
+console.log(num2)
+const pi = 3.14159;
+console.log(pi)*/
+const price = 10;
+console.log(Boolean(price));
+const name = 'james';
+console.log(Boolean(name));
+const num = 10;
+let isTrue = true;
+console.log(isTrue);
+
